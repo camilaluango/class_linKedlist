7e7f379a0c8ccbd6a2fefefb62d97840c019527c
camila <mariacamilaluango@gmail.com>
2025-02-27 23:08:38 -0500

    linkedlist

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-class nodo {
-    constructor(valor){
-        this.valor = valor;
-        this.siguiente = null;
+class node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class linkedlist {
+    constructor(){
+        this.head = null;
+    }
+    aggFinal(value){
+        const nuevoNode = new node(value);
+        if(this.head === null) {
+            this.head = nuevoNode;
+        } else {
+            let actual = this.head;
+            while (actual.next !== null) {
+                actual = actual.next;
+            }
+            actual.next = nuevoNode;
+        }
+    }
+
+    mostrarLista() {
+        let actual = this.head;
+        while (actual !== null){
+            console.log(actual.value);
+            actual = actual.next;
+        }
     }
 }
